6f04b9e7d7eba0075dc4fe1f1334714c00cf49ed
GuilhermeHPSilva <67592214+GuilhermeHenriquePinheiroSilva@users.noreply.github.com>
2024-08-05 01:15:04 -0300

    second commit

---
+++ backend/server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'minadeaco'
+})
+
+app.post('/register', (req, res) => {
+    const name = req.body.name
+    const email = req.body.email
+    const password = req.body.password
+    const confPassword = req.body.confPassword
+    const sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+
+    db.query("SELECT * FROM USERS WHERE EMAIL = ?", [email], (err, data) => {
+        if (data.length > 0) {
+            res.status(201).send("jc");
+            return data
+        }
+        else if (password == confPassword) {
+            db.query(sql, [name, email, password], (err, data) => {
+                if (err) {
+                    res.status(201).send(err)
+                    return data
+                }
+                res.status(201).send("ok")
+                return data
+            })
+        }
+        else {
+            res.status(201).send("si");
+            return
+        }
+    })
+})
+
+app.post('/login', (req, res) => {
+    const email = req.body.email
+    const password = req.body.password
+
+    const sql = "SELECT * FROM USERS WHERE EMAIL = ? AND PASSWORD = ?";
+    db.query(sql, [email, password], (err, data) => {
+        if (err){
+            res.status(201).send(err);
+            return data
+        }
+        else if(data.length > 0){
+            res.status(201).send("ok");
+            return data
+        }
+        else{
+            res.status(201).send("nb");
+            return data
+        }
+    })
+})
+
+app.listen(8081, () => {
+    console.log("Listening...");
+})
---
+++ frontend/src/register.js
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function Register() {
+    const navigate = useNavigate();
+
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPasword] = useState('')
+    const [confPassword, setConfPassword] = useState('')
+    const [response, setResponse] = useState(null);
+
+    const gotoLogin = () => {
+        navigate("/login");
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8081/register', { name, email, password, confPassword })
+            .then((res) => {
+                if(res.data == "jc"){
+                    setResponse("Email já cadastrado.");
+                }
+                else if(res.data == "si"){
+                    setResponse("Senhas não batem.");
+                }
+                else if(res.data == "ok"){
+                    gotoLogin();
+                }
+            })
+    };
+
+    return (
+        <div className='d-flex vh-100 justify-content-center align-items-center bg-primary'>
+            <div className='p-3 bg-white w-25 h-auto'>
+                <center><i className='bi bi-person h1'></i>
+                    <h1 className='mb-300'>Cadastro</h1></center>
+                <form onSubmit={handleSubmit}>
+                    <div className='mb-3 d-flex align-items-center bg-white mb-100'>
+                        <i className="bi bi-person-fill"></i>
+                        <input type="text" placeholder='Nome' className='form-control border-0' onChange={e => setName(e.target.value)} required></input>
+                    </div>
+                    <div className='mb-3 d-flex align-items-center mb-100'>
+                        <i className="bi bi-envelope-at-fill"></i>
+                        <input type="email" placeholder='E-mail' className='form-control border-0' onChange={e => setEmail(e.target.value)} required></input>
+                    </div>
+                    <div className='mb-3 d-flex align-items-center mb-100'>
+                        <i className="bi bi-lock-fill"></i>
+                        <input type="password" placeholder='Senha' className='form-control border-0' onChange={e => setPasword(e.target.value)} required></input>
+                    </div>
+                    <div className='mb-3 d-flex align-items-center mb-100'>
+                        <i className="bi bi-lock-fill"></i>
+                        <input type="password" placeholder='Confirmar Senha' className='form-control border-0' onChange={e => setConfPassword(e.target.value)} required></input>
+                    </div>
+                    <center><a href='/login'>Já possui uma conta? Entrar</a></center>
+                    <p className='text-danger'>{response}</p>
+                    <center><button className='btn btn-success w-100 rounded-0'>Confirmar</button></center>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Register
