ff5c1184d29500ee746e784409a99aa0d844bc85
GuilhermeHPSilva <67592214+GuilhermeHenriquePinheiroSilva@users.noreply.github.com>
2024-09-24 16:02:29 -0300

    DashBoard(NF)

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -11,16 +11,7 @@ function Home() {
         <div>
             <img src={background} alt="Background" className="background" />
             <Navbar />
-            <header className="header">
-                <a href="#" className="logo">Mina De Aço</a>
-                <nav>
-                    <a href="#" className="active">Home</a>
-                    <a href="servicos">Serviços</a>
-                    <a href="sobre">Sobre</a>
-                    <a href="contato">Contato</a>
-                    <a href='register'>Iniciar sessão</a>
-                </nav>
-            </header>
+            
             <section className="home">
                 <div className="homeContent">
                     <h1>Olá, seja bem-vindo(a) ao nosso projeto. <span>Mina De Aço</span></h1>
